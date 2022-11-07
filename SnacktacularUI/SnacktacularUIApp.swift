039d42ef4368f2146aed3bf507489d62617f8981
Johnny Morgan <johnnylion3@aol.com>
2022-11-07 12:29:41 -0500

    Initial Commit

---
+++ SnacktacularUI/SnacktacularUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularUIApp.swift
+//  SnacktacularUI
+//
+//  Created by Johnny Lion on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
