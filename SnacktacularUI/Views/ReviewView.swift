1ca1e4aded68e3ffbffec137386a84ace2d6315e
Johnny Morgan <johnnylion3@aol.com>
2022-11-29 11:59:07 -0500

    Ch. 8.11 Tap for Star Ratings in SwiftUI + Review Details (Snacktacular app)

---
+++ SnacktacularUI/Views/ReviewView.swift
@@ -0,0 +1,92 @@
+//
+//  ReviewView.swift
+//  SnacktacularUI
+//
+//  Created by Johnny Lion on 11/28/22.
+//
+
+import SwiftUI
+
+struct ReviewView: View {
+    @State var spot: Spot
+    @State var review: Review
+    @Environment(\.dismiss) private var dismiss
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                Text(spot.name)
+                    .font(.title)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                .lineLimit(1)
+                
+                Text(spot.address)
+                    .padding(.bottom)
+            }
+            .padding(.horizontal)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Text("Click to Rate:")
+                .font(.title2)
+                .bold()
+            
+            HStack {
+                StarsSelectionView(rating: review.rating)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(.gray.opacity(0.5), lineWidth: 2)
+                    }
+            }
+            .padding(.bottom)
+            
+            VStack(alignment: .leading) {
+                Text("Review Title:")
+                    .bold()
+                
+                TextField("Title", text: $review.title)
+                    .textFieldStyle(.roundedBorder)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(.gray.opacity(0.5), lineWidth: 2)
+                    }
+                Text("Review")
+                    .bold()
+                TextField("review", text: $review.title, axis: .vertical)
+                    .padding(.horizontal, 6)
+                    .frame(maxHeight: .infinity, alignment: .topLeading)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(.gray.opacity(0.5), lineWidth: 2)
+                    }
+                
+            }
+            .padding(.horizontal)
+            .font(.title2)
+            
+            
+            Spacer()
+            
+        }
+        .toolbar {
+            ToolbarItemGroup(placement: .cancellationAction) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                Button("Save") {
+                    //TODO: Add save code here
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+struct ReviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            ReviewView(spot: Spot(name: "Shake Shack", address: "49 Boyleston St., Chestnut Hill, MA 02467"), review: Review())
+        }
+    }
+}
